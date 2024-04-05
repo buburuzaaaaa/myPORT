f46b92a59fdabacd972e7ad5b25ae3c3ec0b1e33
buburuzaaaaa <andrew4joy2015@gmail.com>
2024-04-05 15:15:47 -0700

    update

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+import './App.css';
+import Navbar  from './components/Navbar';
+function App() {
+  return (
+    <div className="App">
+     <Navbar />
+     <div className="content-container">
+        <h1>Hi, I'm Andrew Silaghi</h1>
+        <button className="right-btn">Contact</button>
+      </div>
+      <h3>Frontend web designer </h3>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates quasi soluta commodi magnam magni. Accusantium explicabo sit repellat omnis obcaecati itaque sequi iste id esse dolores ullam eligendi similique, fugit alias voluptates non aliquam, a iure dolor! Modi, culpa porro a ad in adipisci suscipit quia dolorum maxime aspernatur iusto. Perspiciatis at atque distinctio, voluptas minima laborum commodi reiciendis. Quo, deleniti voluptatem! Possimus reprehenderit quod, totam deleniti, omnis vel similique vitae eum laudantium aspernatur repellendus velit quos magnam tenetur non! Necessitatibus debitis quasi reprehenderit. Ab voluptatum consectetur ex cupiditate delectus, inventore corrupti provident consequatur! Quos incidunt facilis beatae ea quae?</p>
+    </div>
+  );
+}
+
+export default App;
